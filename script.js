39d64c199f3025807daa13d3f2c931908104a9ee
Mahir1401 <mahirrahman1122@gmail.com>
2026-03-15 16:14:44 +0600

    login page done

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ loginBtn.addEventListener('click',function(){
   let passwordValue = passwordInput.value;
   if(usernameValue === 'admin' && passwordValue === 'admin123'){
     alert('Login Successful');
-    window.location.assign("/dashboard.html");
+    window.location.assign("./dashboard.html");
   }
   else{
     alert('Login Failed');
     return;
   }
-})
+});
 
 const allFilterBtn = document.getElementById('All-filter-btn');
 const openFilterBtn = document.getElementById('Open-filter-btn');
@@ -52,3 +52,11 @@ function toggle(name) {
     closedSection.classList.remove('hidden')
   }
 }
+
+const loadIssues = ()=>{
+  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+  .then((res) => res.json())
+  .then((json)=> console.log(json));
+};
+
+loadIssues();
